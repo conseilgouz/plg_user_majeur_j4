906c9549de6c4cbf62f819ef894758d59e2227d0
Pascal Leconte <pascal.leconte@conseilgouz.com>
2025-01-24 16:47:35 +0100

    2.0.1
    Missing class variables

--- src/Extension/Majeur.php
+++ src/Extension/Majeur.php
@@ -5,6 +5,7 @@
  * @copyright (c) 2025 ConseilGouz. All Rights Reserved.
  * @author ConseilGouz
  */
+
 namespace ConseilGouz\Plugin\User\Majeur\Extension;
 
 defined('_JEXEC') or die;
@@ -19,6 +20,9 @@ use Joomla\Event\SubscriberInterface;
 
 class Majeur extends CMSPlugin implements SubscriberInterface
 {
+    protected $date;
+    protected $_subject;
+
     public static function getSubscribedEvents(): array
     {
         return [
@@ -27,77 +31,67 @@ class Majeur extends CMSPlugin implements SubscriberInterface
         ];
     }
 
-	public function onUserBeforeSave($event) // ($user, $isnew, $data)
-	{
+    public function onUserBeforeSave($event) // ($user, $isnew, $data)
+    {
         $data = $event[2];
-		// Check that the date is valid.
-		if (!empty($data['profile']['dob']))
-		{
-			try
-			{
-				$date = new Date($data['profile']['dob']);
-				$this->date = $date->format('Y-m-d H:i:s');
-			}
-			catch (\Exception $e)
-			{
-				// Throw an exception if date is not valid.
-				throw new InvalidArgumentException(Text::_('PLG_USER_PROFILE_ERROR_INVALID_DOB'));
-			}
-			if (Date::getInstance('now') < $date)
-			{
-				// Throw an exception if dob is greather than now.
-				throw new InvalidArgumentException(Text::_('PLG_USER_PROFILE_ERROR_INVALID_DOB'));
-			}
-		}
+        // Check that the date is valid.
+        if (!empty($data['profile']['dob'])) {
+            try {
+                $date = new Date($data['profile']['dob']);
+                $this->date = $date->format('Y-m-d H:i:s');
+            } catch (\Exception $e) {
+                // Throw an exception if date is not valid.
+                throw new \InvalidArgumentException(Text::_('PLG_USER_PROFILE_ERROR_INVALID_DOB'));
+            }
+            if (Date::getInstance('now') < $date) {
+                // Throw an exception if dob is greather than now.
+                throw new \InvalidArgumentException(Text::_('PLG_USER_PROFILE_ERROR_INVALID_DOB'));
+            }
+        }
 
 
-		return true;
-	}
-	/**
-	 * UPDATE username with UserId
-	 *
-	 * @param   array    $data    entered user data
-	 * @param   boolean  $isNew   true if this is a new user
-	 * @param   boolean  $result  true if saving the user worked
-	 * @param   string   $error   error message
-	 *
-	 * @return bool
-	 */
-	public function onUserAfterSave($event) // ($data, $isNew, $result, $error)
-	{
+        return true;
+    }
+    /**
+     * UPDATE username with UserId
+     *
+     * @param   array    $data    entered user data
+     * @param   boolean  $isNew   true if this is a new user
+     * @param   boolean  $result  true if saving the user worked
+     * @param   string   $error   error message
+     *
+     * @return bool
+     */
+    public function onUserAfterSave($event) // ($data, $isNew, $result, $error)
+    {
         $data = $event[0];
         $isNew = $event[1];
         $result = $event[2];
-        
-		$userId = ArrayHelper::getValue($data, 'id', 0, 'int');
 
-		if ($isNew && $userId && $result && isset($data['profile']) && (count($data['profile'])) && ($this->params->def('major_usergroup')) && ($this->date))
-		{  
-		    $group = $this->params->def('major_usergroup');
-			try
-			{
-				// Sanitize the date
-				$data['profile']['dob'] = $this->date;
-				$mineur = new Date('now -18 year');
-				if ($data['profile']['dob'] > $mineur->format('Y-m-d H:m:s')) { 
-					return true;
-				}
-				// on est sur majeur
-				$db = Factory::getContainer()->get(DatabaseInterface::class);
-				$sql = 'INSERT INTO #__user_usergroup_map VALUES ('.$userId.','.$group.')';
-				$db->setQuery($sql);
-				$db->execute();
-			}
-			catch (RuntimeException $e)
-			{
-				$this->_subject->setError($e->getMessage());
+        $userId = ArrayHelper::getValue($data, 'id', 0, 'int');
 
-				return false;
-			}
-		}
+        if ($isNew && $userId && $result && isset($data['profile']) && (count($data['profile'])) && ($this->params->def('major_usergroup')) && ($this->date)) {
+            $group = $this->params->def('major_usergroup');
+            try {
+                // Sanitize the date
+                $data['profile']['dob'] = $this->date;
+                $mineur = new Date('now -18 year');
+                if ($data['profile']['dob'] > $mineur->format('Y-m-d H:m:s')) {
+                    return true;
+                }
+                // on est sur majeur
+                $db = Factory::getContainer()->get(DatabaseInterface::class);
+                $sql = 'INSERT INTO #__user_usergroup_map VALUES ('.$userId.','.$group.')';
+                $db->setQuery($sql);
+                $db->execute();
+            } catch (\Exception $e) {
+                $this->_subject->setError($e->getMessage());
 
-		return true;
-	}
+                return false;
+            }
+        }
 
+        return true;
+    }
 
 }
